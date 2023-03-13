ef4779b5a0103a8b84869d4d089e98bc361ad480
Ahsan Ullah Shakil <91214421+ahsanshakil-baust@users.noreply.github.com>
2023-03-13 19:09:45 +0600

    un used variable removed

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const auth = require("./auth");
 const multer = require("multer");
 const bcrypt = require("bcryptjs");
 
-const name="Ahsan";
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 

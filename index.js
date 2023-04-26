281370a4a9f12750c41a6c8cd18c86351d96da7b
Ahsan Ullah Shakil <91214421+ahsanshakil-baust@users.noreply.github.com>
2023-04-26 22:01:20 +0600

    updated

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const auth = require("./auth");
 const multer = require("multer");
 const bcrypt = require("bcryptjs");
 
-const sample="kopalpor";
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 

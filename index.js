0fe499069de4860ee4cad10e31c9573732865e5b
Ahsan Ullah Shakil <91214421+ahsanshakil-baust@users.noreply.github.com>
2023-05-25 22:11:17 +0600

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const auth = require("./auth");
 const multer = require("multer");
 const bcrypt = require("bcryptjs");
 
+const api="app";
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
